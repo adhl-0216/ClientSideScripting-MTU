56fdc4bcc26ce45d3efd3fcd8cd8c23f6087258c
mtu-adhl <113506757+mtu-adhl@users.noreply.github.com>
2022-09-21 08:58:41 +0100

    Delete Client Side Scripting directory

--- Client Side Scripting/HelloThere.js
+++
@@ -1,2 +0,0 @@
-// JavaScript Document
-document.write("<em> General Kenobi. </em>");
--- Client Side Scripting/lesson1.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-function inlineScript(){
-    let msg = "Hello there!";
-    alert(msg);
-}
-
-inlineScript();
