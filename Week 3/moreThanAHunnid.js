0693b110c3eecf1547ceebb0f0d5e124110a631f
mtu-adhl <113506757+mtu-adhl@users.noreply.github.com>
2022-09-28 12:59:57 +0100

    220928
    do dumb method for Farenheit to Celcius conversion

---
+++ Week 3/moreThanAHunnid.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+    'use strict'
+    
+    function moreThanAHunnid(num){
+       return (num > 100)?"Greater than 100":"Less than 100";
+    };
+    
+    let num;
+    num = parseInt(window.prompt("Enter a number"));
+    
+    alert(moreThanAHunnid(num));
+    
