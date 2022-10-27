5c3770aecdac8689c4be9b39e007844bb85ee73e
adhl <Han.Lim.Oah@students.ittralee.ie>
2022-10-27 10:08:37 +0100

    221027

--- Week 3/moreThanAHunnid.js
+++ Week 3/moreThanAHunnid.js
@@ -3,7 +3,7 @@
     
     function moreThanAHunnid(num){
        return (num > 100)?"Greater than 100":"Less than 100";
-    };
+    }
     
     let num;
     num = parseInt(window.prompt("Enter a number"));
