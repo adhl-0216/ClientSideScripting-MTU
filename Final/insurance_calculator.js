2355fc7c1deb0d0fb17e770b3c9dbc83ad9b42f6
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-11-29 16:59:26 +0000

    done html

--- Final/insurance_calculator.js
+++ Final/insurance_calculator.js
@@ -1,3 +1,39 @@
-$(document).ready(function (){
-    $("input[type='text'][readonly]").val("0.00");
-});
+let allListBox = document.querySelectorAll("select");
+for (let i = 0; i < allListBox.length; i++) {
+    allListBox[i].selectedIndex = -1;
+}
+
+/**
+ * button onclick event handler
+ * */
+let calculate = function (e){
+    /**
+     * input validation
+     */
+    let output = "";
+
+    let allText = document.querySelectorAll("input[type='text']:not([id='running-total'])");
+    for (const text of allText) {
+        if (text.value === ""){
+            alert("All Fields Must Be Entered!");
+            text.focus();
+            break;
+        }else {
+            output += text.id + ": " + text.value + "\n";
+        }
+    }
+
+    let allNumerics = document.querySelectorAll(".numeric");
+    for (const numeric of allNumerics) {
+        if (isNaN(Number(numeric.value)) || numeric.value.indexOf('.') !== -1) {
+            alert("Please enter whole numbers only!");
+            numeric.value = "";
+        }else {
+            //output += numeric.value + "\n";
+        }
+    }
+
+    alert(output);
+}
+document.querySelector("button").addEventListener("click", calculate);
+
