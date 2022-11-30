74c406daa332ee51f94892367532b586f9132926
ADHL <Han.Lim.Oah@students.ittralee.ie>
2022-11-30 12:52:26 +0000

    week 11 and FINAL CA

--- FINAL CA/insurance_calculator.js
+++ FINAL CA/insurance_calculator.js
@@ -1,4 +1,4 @@
-let allListBox = document.querySelectorAll("select");
+const allListBox = document.querySelectorAll("select");
 for (let i = 0; i < allListBox.length; i++) {
     allListBox[i].selectedIndex = -1;
 }
@@ -12,7 +12,7 @@ let calculate = function (e){
      */
     let output = "";
 
-    let allText = document.querySelectorAll("input[type='text']:not([id='running-total'])");
+    const allText = document.querySelectorAll("input[type='text']:not([id='running-total'])");
     for (const text of allText) {
         if (text.value === ""){
             alert("All Fields Must Be Entered!");
@@ -23,7 +23,7 @@ let calculate = function (e){
         }
     }
 
-    let allNumerics = document.querySelectorAll(".numeric");
+    const allNumerics = document.querySelectorAll(".numeric");
     for (const numeric of allNumerics) {
         if (isNaN(Number(numeric.value)) || numeric.value.indexOf('.') !== -1) {
             alert("Please enter whole numbers only!");
@@ -36,4 +36,13 @@ let calculate = function (e){
     alert(output);
 }
 document.querySelector("button").addEventListener("click", calculate);
+
+// const allAsterisk = document.querySelectorAll(".tip");
+// let showTooltip = function (e){
+//     console.log(e.target.firstElementChild);
+//     e.target.firstElementChild.style.visibility = "visible";
+// }
+// for (const ast of allAsterisk) {
+//     ast.addEventListener("mouseover", showTooltip);
+// }
 
