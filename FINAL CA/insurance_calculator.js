f547a0a811295409e1fd177c52047509ee4368fc
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-12-14 23:28:41 +0000

    Price Analysis.html WIP

--- FINAL CA/insurance_calculator.js
+++ FINAL CA/insurance_calculator.js
@@ -7,71 +7,71 @@ const form = document.forms[0];
 
 const runningTotal = document.getElementById("running-total");
 
-const txtEmail = document.querySelector("#email");
-let valid = false;
-let validateEmail = function () {
-    const mailFormat = /(?:[a-z\d!#$%&'*+=?^_`{|}~-]+(?:\.[a-z\d!#$%&'*+=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z\d](?:[a-z\d-]*[a-z\d])?\.)+[a-z\d](?:[a-z\d-]*[a-z\d])?|\[(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?|[a-z\d-]*[a-z\d]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/;
-    if (!txtEmail.value.match(mailFormat)) {
-        alert("Invalid Email Entered!");
-        txtEmail.focus();
-        return false;
-    }
-    return true;
-};
-txtEmail.addEventListener("change", validateEmail);
-
-const txtBoxes = document.querySelectorAll("input[pattern]");
+const txtBoxes = document.querySelectorAll("input[pattern='[0-9]*']");
 const radioButtons = document.querySelectorAll("input[type='radio']");
 const selectBoxes = document.querySelectorAll("select");
 
-console.log(selectBoxes);
+let resetOutline = function (e) {
+    e.target.style.outline = "none";
+    e.target.removeEventListener("focusout", resetOutline);
+}
 
 let updateTotal = function () {
-    let init = 0;
+    let price = 0;
 
     for (const radioButton of radioButtons) {
         if (radioButton.checked === true){
-            init += Number(radioButton.value);
+            price += Number(radioButton.value);
         }
     }
 
     for (const txtBox of txtBoxes) {
-        let price;
-        if (txtBox.id === "years-claim-free") price = Number(txtBox.value)*-10;
-        else price = Number(txtBox.value)*10;
-        init += price;
+        if (isNaN(Number(txtBox.value))) {
+            txtBox.style.outline = "1px solid red";
+            txtBox.addEventListener("focusout", resetOutline);
+            txtBox.focus();
+            return;
+        }
+        if (txtBox.id === "years-claim-free") price += Number(txtBox.value) * -10;
+        else price += Number(txtBox.value) * 10;
     }
 
     for (const selectBox of selectBoxes) {
         if (selectBox.id === "contents-cover") {
-            init += Number(selectBox.value*10);
+            price += Number(selectBox.value*10);
             continue;
         }
 
         if (selectBox.id === "building-cover") {
-            init += Number(selectBox.value*20);
+            price += Number(selectBox.value*20);
             continue;
         }
 
-        init += Number(selectBox.value);
+        price += Number(selectBox.value);
     }
 
-    runningTotal.value = '\u20AC ' + Number(init).toFixed(2);
+    runningTotal.value = '\u20AC ' + Number(price).toFixed(2);
 }
 
-form.addEventListener("focusout", updateTotal);
+form.addEventListener("focus", updateTotal);
 form.addEventListener("change", updateTotal);
 
-let calculate = function (e){
-    e.preventDefault()
-    if(validateEmail()) valid = true;
-    if (valid){
-        form.submit();
+const txtEmail = document.querySelector("#email");
+
+let submitForm = function (e){
+    e.preventDefault();
+    let mailFormat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    if(!txtEmail.value.match(mailFormat))
+    {
+        txtEmail.style.outline = "1px solid red";
+        txtEmail.addEventListener("focusout", resetOutline);
+        txtEmail.focus();
+        return;
+    }
+    form.submit();
+    for (let i = 0; i < form.length; i++) {
+        form[i].disabled = true;
     }
 };
 
-document.querySelector("#calculate").addEventListener("submit", calculate);
-
-
-
-
+form.addEventListener("submit", submitForm);
