994c533208dc381f55e72d8e8eafa9e06c9720cd
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-12-15 17:15:47 +0000

    100%!

--- FINAL CA/price_analysis.js
+++ FINAL CA/price_analysis.js
@@ -9,95 +9,105 @@ for (let string of arr) {
     let strArr = string.split("=");
     obj[strArr[0].toString()] = strArr[1];
 }
-(obj["ownership"] === "50")? obj["ownership"] = "Tenant": obj["ownership"] = "Owner";
-(obj["area"] === "50")? obj["area"] = "Outside Dublin": obj["area"] = "Dublin";
+if (obj["ownership"] === "50") {
+    obj["ownership"] = "Tenant (\u20ac " + obj["ownership"] + ")";
+} else {
+    obj["ownership"] = "Owner (\u20ac " + obj["ownership"] + ")";
+}
+
+if (obj["area"] === "50") {
+    obj["area"] = "Outside Dublin (\u20ac " + obj["area"] + ")";
+} else {
+    obj["area"] = "Dublin (\u20ac " + obj["area"] + ")";
+}
 
 switch (obj["property type"]) {
     case "75" :{
-        obj["property type"] = "Bungalow";
+        obj["property type"] = "Bungalow (\u20ac " + obj["property type"]+ ")";
         break;
     }
     case "100" :{
-        obj["property type"] = "Country House";
+        obj["property type"] = "Country House (\u20ac " + obj["property type"]+ ")";
         break;
     }
     case "50" :{
-        obj["property type"] = "Detached";
+        obj["property type"] = "Detached (\u20ac " + obj["property type"]+ ")";
         break;
     }
     case "40" :{
-        obj["property type"] = "Semi Detached";
+        obj["property type"] = "Semi Detached (\u20ac " + obj["property type"]+ ")";
         break;
     }
     case "30" :{
-        obj["property type"] = "Terraced";
+        obj["property type"] = "Terraced (\u20ac " + obj["property type"]+ ")";
         break;
     }
     case "20" :{
-        obj["property type"] = "Flat";
+        obj["property type"] = "Flat (\u20ac " + obj["property type"]+ ")";
         break;
     }
 }
 
 switch(obj["cover type"]){
     case "50":{
-        obj["cover type"] = "Owner Occupied";
+        obj["cover type"] = "Owner Occupied (\u20ac " + obj["cover type"] + ")";
         break;
     }
     case "60":{
-        obj["cover type"] = "Holiday";
+        obj["cover type"] = "Holiday (\u20ac " + obj["cover type"] + ")";
         break;
     }
     case "70":{
-        obj["cover type"] = "Rental";
+        obj["cover type"] = "Rental (\u20ac " + obj["cover type"] + ")";
         break;
     }
 }
 
 switch (obj["contents cover"]){
     case "1":{
-        obj["contents cover"] = "\u20ac 0 - \u20ac 5,000";
+        obj["contents cover"] = "\u20ac 0 - \u20ac 5,000 (\u20ac " + obj["contents cover"]*10 + ")";
         break;
     }
     case "2": {
-        obj["contents cover"] = "\u20ac 5,001 - \u20ac 10,000";
+        obj["contents cover"] = "\u20ac 5,001 - \u20ac 10,000 (\u20ac " + obj["contents cover"]*10 + ")";
         break;
     }
     case "3": {
-        obj["contents cover"] = "\u20ac 10,001 - \u20ac 15,000";
+        obj["contents cover"] = "\u20ac 10,001 - \u20ac 15,000 (\u20ac " + obj["contents cover"]*10 + ")";
         break;
     }
     case "4": {
-        obj["contents cover"] = "\u20ac 15,001 - \u20ac 20,000";
+        obj["contents cover"] = "\u20ac 15,001 - \u20ac 20,000 (\u20ac " + obj["contents cover"]*10 + ")";
         break;
     }
     case "5": {
-        obj["contents cover"] = "\u20ac 20,001 - \u20ac 25,000";
+        obj["contents cover"] = "\u20ac 20,001 - \u20ac 25,000 (\u20ac " + obj["contents cover"]*10 + ")";
         break;
     }
 }
 switch (obj["building cover"]){
     case "1":{
-        obj["building cover"] = "\u20ac 0 - \u20ac 100,000";
+        obj["building cover"] = "\u20ac 0 - \u20ac 100,000 (\u20ac " + obj["building cover"]*20 + ")";
         break;
     }
     case "2": {
-        obj["building cover"] = "\u20ac 100,001 - \u20ac 200,000";
+        obj["building cover"] = "\u20ac 100,001 - \u20ac 200,000 (\u20ac " + obj["building cover"]*20 + ")";
         break;
     }
     case "3": {
-        obj["building cover"] = "\u20ac 200,001 - \u20ac 300,000";
+        obj["building cover"] = "\u20ac 200,001 - \u20ac 300,000 (\u20ac " + obj["building cover"]*20 + ")";
         break;
     }
     case "4": {
-        obj["building cover"] = "\u20ac 300,001 - \u20ac 400,000";
+        obj["building cover"] = "\u20ac 300,001 - \u20ac 400,000 (\u20ac " + obj["building cover"]*20 + ")";
         break;
     }
     case "5": {
-        obj["building cover"] = "\u20ac 400,001 - \u20ac 500,000";
+        obj["building cover"] = "\u20ac 400,001 - \u20ac 500,000 (\u20ac " + obj["building cover"]*20 + ")";
         break;
     }
 }
+
 
 let keys = Object.keys(obj);
 let i = 0;
@@ -105,23 +115,32 @@ for (const key of keys) {
 
     let p = document.createElement("p");
 
-    if(i < keys.length-1){
+    if (i < keys.length-1){
         p.className = "field half";
     }else {
         p.className = "field";
     }
 
-    let cell1 = document.createElement("label");
-    cell1.className = "label";
-    let cell2 = document.createElement("input");
-    cell2.type = "text";
-    cell2.readOnly = true;
-    cell2.className = "text-input";
 
-    cell1.innerHTML = key.toUpperCase();
-    cell2.value = obj[key];
-    p.replaceChildren(cell1, cell2);
+    let label = document.createElement("label");
+    label.className = "label";
+    let textBox = document.createElement("input");
+    textBox.type = "text";
+    textBox.readOnly = true;
+    textBox.className = "text-input"
+
+    let priceDetail;
+    if (key === "no of bedrooms" || key === "years built") {
+        priceDetail = obj[key] + " (\u20ac " + obj[key]*10 + ")";
+    }else if (key === "years claim free"){
+        priceDetail = obj[key] + " (\u20ac " + obj[key]*10 + " off)";
+    }
+    else priceDetail = obj[key];
+    label.innerHTML = key.toUpperCase();
+    textBox.value = priceDetail;
+    p.replaceChildren(label, textBox);
 
     form.appendChild(p);
+    form.appendChild(document.createElement("br"));
     i++;
 }
